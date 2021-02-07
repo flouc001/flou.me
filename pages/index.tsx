54f1acabaec5463909d886c2aae72bcb36f28cd7
flouc001 <flouc001@gmail.com>
2021-02-07 19:33:06 +0000

    make mobile friendly

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { AnimatedText } from '@components/AnimatedText';
 
 const Home: React.FC<{}> = () => (
-  <div className="flex flex-grow h-screen justify-center items-center text-8xl">
+  <div className="flex flex-grow h-screen justify-center items-center text-4xl lg:text-8xl">
     <AnimatedText text="Coming soon..." />
   </div>
 );
