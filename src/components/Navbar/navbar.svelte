136b573cd67fdb7ad610de21c30cc92a2bb629ab
Callum Floume <flouc001@gmail.com>
2022-02-05 18:07:17 +0000

    feat: add navbar skeleton

---
+++ src/components/Navbar/navbar.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { page } from '$app/stores'
+
+  const navLinks = [
+    { label: 'Home', path: '/' },
+    { label: 'Blog', path: '/blog' },
+    { label: 'About', path: '/about' }
+  ]
+</script>
+
+<nav class="bg-blue-900">
+  <div class="relative flex items-center justify-end h-16 px-4 space-x-2">
+    {#each navLinks as link}
+      {@const isActive = $page.url.pathname === link.path}
+      <a
+        class="px-3 py-2 text-sm {isActive ? 'text-white' : 'text-blue-300'}"
+        href="{link.path}"
+      >{link.label}</a>
+    {/each}
+  </div>
+</nav>
