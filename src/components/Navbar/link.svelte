f05d6299667038510dadc63493be0c0b94d913f5
Callum Floume <flouc001@gmail.com>
2022-02-06 21:54:15 +0000

    style: navbar

---
+++ src/components/Navbar/link.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { NavLink } from './types'
+
+  export let isActive: boolean = false;
+  export let link: NavLink;
+</script>
+
+<div
+  class="flex text-md text-white box-border h-8 items-center border-transparent border-y-2 hover:border-b-blue-500 transition"
+  class:border-b-blue-500={isActive}
+>
+  <a
+    href="{link.path}"
+    class="px-3"
+  >{link.label}</a>
+</div>
